8c8a7220d9b953e8babc84bd50bfe96f6eaf36e8
Jenna Bushspies <jenna.bushspies+1@dbtlabs.com>
2025-11-21 15:17:43 +0000

    test

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,3 +1,4 @@
-{%  macro cents_to_dollars(column_name, decimals=2) %}
-    round({{ column_name }} * 1.0 / 100, {{ decimals }})
+{%  macro cents_to_dollars(column_name, decimals=4) %}
+    ({{ column_name }} * 1.0 / 100)::decimal(10, {{ decimals }})
 {%- endmacro %}
+
